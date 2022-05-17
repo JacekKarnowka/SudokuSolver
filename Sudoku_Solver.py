b66277fcdbbafbba29296b87be7903afea36dd90
Jacek Karnowka <jacek.karnowka@gmail.com>
2022-05-17 15:28:16 +0200

    Change input value as a .txt file with digits, as in example file. Change program structure to class based.

--- Sudoku_Solver.py
+++ Sudoku_Solver.py
@@ -1,55 +1,81 @@
-sudoku_to_solve = [[5,3,0,0,7,0,0,0,0],
-                  [6,0,0,1,9,5,0,0,0],
-                  [0,9,8,0,0,0,0,6,0],
-                  [8,0,0,0,6,0,0,0,3],
-                  [4,0,0,8,0,3,0,0,1],
-                  [7,0,0,0,2,0,0,0,6],
-                  [0,6,0,0,0,0,2,8,0],
-                  [0,0,0,4,1,9,0,0,5],
-                  [0,0,0,0,8,0,0,7,9]]
+class sudoku_board():
+    def __init__(self):
+        # Load sudoku board from "Sudoku_to_solve.txt" file
+        try:
+            with open("./Sudoku_to_solve.txt") as f:
+                lines = f.readlines()
 
-def check_sudoku(board, digit, row, col):
+            rows = [i.split("\n")[0] for i in lines]
 
-    def check_rows(board, digit, row):
-        return False if digit in board[row] else True
+            sudoku = [[int(j) for j in i.split(",")] for i in rows]
+
+            for check_rows in rows:
+                if len(check_rows) != 17:
+                    raise Exception("Wrong lenght of data!")
+        
+            if len(sudoku) != 9:
+                raise Exception("Wrong lenght of data!")
+
+            self.board = sudoku
+
+        except Exception as err:
+            print("Error: {}".format(err))
+            return None
+
+    # Print board function
+    def print_board(self):
+        for row in self.board:
+            print("{}".format(row))
+
     
-    def check_columns(board, digit, col):
+    def check_sudoku(self, digit, row, col):
 
-        return False if digit in [i[col] for i in board] else True
+        if (self.check_rows(digit, row) and self.check_columns(digit, col) and self.check_segment(digit, row, col)):
+            return True
+        else:
+            return False
 
-    def check_segment(board, digit, row, col):
+    def check_rows(self, digit, row):
+        return False if digit in self.board[row] else True
+    
+    def check_columns(self, digit, col):
+        return False if digit in [i[col] for i in self.board] else True
+
+    def check_segment(self, digit, row, col):
         i = row - row % 3
         j = col - col % 3
-        nums = board[i][j:j + 3] + board[i + 1][j:j + 3] + board[i + 2][j:j + 3]        
+        nums = self.board[i][j:j + 3] + self.board[i + 1][j:j + 3] + self.board[i + 2][j:j + 3]     
+            
         return False if digit in nums else True
        
-    if (check_rows(board, digit, row) and check_columns(board, digit, col) and check_segment(board, digit, row, col)):
-        return True
-    else:
-        return False 
-
-def solver(board):
+         
+    # Backtracking algorithm solver
+    def solver(self):
     
-    for row in range(0, 9): # iterating over rows
-        for col in range (0, 9): # iterating over columns
+        for row in range(0, 9): # iterating over rows
+            for col in range (0, 9): # iterating over columns
             
-            if (board[row][col] == 0): # checking if board[row][column] is 0 
-                for digit in range(1, 10): # loop over possible values from 1 to 9
+                if (self.board[row][col] == 0): # checking if board[row][column] is 0 
+                    for digit in range(1, 10): # loop over possible values from 1 to 9
 
-                    if(check_sudoku(board, digit, row, col) == True): # check if value "digit" from loop above is correct
-                        board[row][col] = digit # if value "digit" is correct, try to put value in board[row][col]
-                        
-                        if (solver(board) == True):
-                            return True
-                        else:
-                            board[row][col] = 0
+                        if(self.check_sudoku(digit, row, col) == True): # check if value "digit" from loop above is correct
+                            
+                            self.board[row][col] = digit # if value "digit" is correct, try to put value in board[row][col]
 
-                return False
-        
-    return True
+                            if (self.solver() == True): # backtrack for another field
+                                return True
+                            else:
+                                self.board[row][col] = 0 # if solution is wrong, back to previous empty field
+                    
+                    return False
 
-print("Sudoku board before solving: \n {}".format(sudoku_to_solve))
+        return True
+ 
 
-solved = solver(sudoku_to_solve)
+board_1 = sudoku_board()
+if board_1.solver():
+    print("Sudoku solved!")
+    board_1.print_board()
+else:
 
-print("Sudoku board after solving: \n {}".format(sudoku_to_solve))
+    print("Sudoku unsolvable")
